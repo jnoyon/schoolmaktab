d8bdc4596c2aa8c687f539bce9965ed2e17e8dc9
jnoyon <jihadur51@gmail.com>
2024-09-17 15:02:48 +0600

    section edited and js files added

--- js/script.js
+++ js/script.js
@@ -31,5 +31,21 @@ document.addEventListener('DOMContentLoaded', function() {
         toggleSidebar();
     });
 
+
+    document.addEventListener('scroll', function() {
+        const nav = document.getElementById('stickyNav');
+        const aboutSection = document.getElementById('about');
+        const navHeight = nav.offsetHeight;
+        const sectionOffsetTop = aboutSection.offsetTop;
+
+        if (window.scrollY > sectionOffsetTop - navHeight) {
+            nav.classList.remove('hidden');
+            nav.classList.add('sticky-nav');
+        } else {
+            nav.classList.add('hidden');
+            nav.classList.remove('sticky-nav');
+        }
+    });
+
     
 });
